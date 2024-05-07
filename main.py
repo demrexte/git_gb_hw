4b317a29857f745bb07b11acd10d6e0beecdc143
demrexte <169113841+demrexte@users.noreply.github.com>
2024-05-07 05:35:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    transposed = [[0 for row in range(rows)] for col in range(cols)]
+
+    for row in range(rows):
+        for col in range(cols):
+            transposed[col][row] = matrix[row][col]
+    return transposed
+
+
+matrix = [[1, 2, 3],
+         [4, 5, 6], 
+         [7, 8, 9]]
+transposed_matrix = transpose(matrix)
